5c8b56a9ed077e37986e4f3d0a841aa291433bd9
Giulia Armillei <giuliaarm00@icloud.com>
2026-03-02 13:08:37 +0100

    Update R_script.R

--- Code/R_script.R
+++ Code/R_script.R
@@ -14,8 +14,8 @@ michela^tecla
 sonia <- c(10, 8, 3, 1, 0)
 giorgia <- c(3, 10, 20, 50, 100)
 plot(giorgia, sonia)
-plot(giorgia, sonia, col="blue", phc=19)
-plot(giorgia, sonia, pch=19, cex=2, xlab="pollution", ylab="numero di delfini")
+plot(giorgia, sonia, col="blue", pch=19)
+plot(giorgia, sonia, pch=19, cex=2, xlab="pollution", ylab="numero di delfini", col="blue")
 
 # installazione pacchetti da CRAN
 install.packages("terra")
