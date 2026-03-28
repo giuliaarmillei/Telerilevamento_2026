2c0d96bd46335ebefc6a056258a6e8b4cbe5ac0f
Giulia Armillei <giuliaarm00@icloud.com>
2026-03-28 19:00:28 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -1,1 +1,21 @@
+library(terra)
+library(imageRy)
 
+im.list()
+EN_01 <- im.import("EN_01.png"))
+plot(EN_01)
+EN_01 <- flip(EN_01)
+plot(EN_01)
+EN_13 <- im.import("EN_03.png")
+EN_13 <- flip(EN_13)
+plot(EN_13)
+
+#esercizio: osservare le due immagini una dopra l'altra
+par(mfrow=c(2,1))
+plot(EN_01)
+plot(EN_13)
+
+#differencing
+ENdif <- EN_01[[1]] - EN_13[[1]] 
+dev.off()
+plot(ENdif)
