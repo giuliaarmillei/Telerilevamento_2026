9b487c170ad651b4b0e49efc23b07571788b9ac4
Giulia Armillei <giuliaarm00@icloud.com>
2026-03-28 20:24:03 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -1,5 +1,6 @@
 library(terra)
 library(imageRy)
+library(viridis)
 
 im.list()
 EN_01 <- im.import("EN_01.png"))
@@ -19,3 +20,16 @@ plot(EN_13)
 ENdif <- EN_01[[1]] - EN_13[[1]] 
 dev.off()
 plot(ENdif)
+
+#esercizio: importa tutte le greenland immagini e crea lo stack
+g2000 <- im.import("greenland.2000.tif")
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+sg <- c(g2000, g2005, g2010, g2015)
+
+gr <- im.import("greenland")
+par(mfrow=c(1,2))
+plot(gr[[1]], col=plasma(100))
+plot(gr[[4]], col=plasma(100))
+
