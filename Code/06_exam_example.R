ed74fb2056474a0b31dd6abe6e4a9db177ece121
Giulia Armillei <giuliaarm00@icloud.com>
2026-04-20 19:25:24 +0200

    Update 06_exam_example.R

--- Code/06_exam_example.R
+++ Code/06_exam_example.R
@@ -1,4 +1,5 @@
 library(terra)
+library(imageRy)
 
 setwd("C:/Users/Giulia/Downloads/drone")
 getwd()
@@ -25,5 +26,13 @@ png("bande.png")
 par(mfrow=c(2,1))
 plot(richat[[1]])
 plot(richat[[2]])
+dev.off()
+
+#istogrammi
+png("ist.png")
+par(mfrow=c(3,1))
+hist(values(richat[[1]]), main="Istogramma Red", col="red")
+hist(values(richat[[2]]), main="Istogramma Green", col="green")
+hist(values(richat[[3]]), main="Istogramma Blue", col="blue")
 dev.off()
 
