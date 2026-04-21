daff056a494f6466051044f6eac73a03515a9891
Giulia Armillei <giuliaarm00@icloud.com>
2026-04-21 17:49:40 +0200

    Update 07_classification.R

--- Code/07_classification.R
+++ Code/07_classification.R
@@ -35,3 +35,59 @@ plot(sir)
 # classify data
 sirc <- im.classify(sir, num_clusters = 3)
 sirc <- im.classify(sir, num_clusters = 5)
+
+# Mato grosso example
+im.list()
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+
+par(mfrow=c(2,1))
+plot(m1992)
+plot(m2006)
+
+# classifichiamo l'immagine del 1992
+m1992c <- im.classify(m1992, num_clusters = 2, seed=42)
+
+#Assign labels: creiamo un dataframe con una colonna con valori 1 e 2 e una colonna con le label "forest" e "human" --> inserisco le etichette come due livelli nella mappa
+
+levels(m1992c) <- data.frame(
+  value = c(1, 2),
+  label = c("forest", "human")
+)
+
+plot(m2006c)
+
+# faccio lo stesso per m2006
+
+m2006c <- im.classify(m2006, num_clusters = 2, seed=42)
+
+# Assign labels
+levels(m2006c) <- data.frame(
+  value = c(2, 1),
+  label = c("forest", "human")
+)
+
+plot(m2006c)
+
+# calculating frequencies con la funzioen freq
+f1992 <- freq(m1992c)
+
+# percentuale --> frequenze/numero totale di celle = proporzione di foreste e umani x 100
+prop1992 <- f1992$count / ncell(m1992c)
+perc1992 <- prop1992*100
+
+#facciamo lo stesso epr il 2006
+f2006 <- freq(m2006c)
+prop2006 <- f2006$count / ncell(m2006c)
+perc2006 <- prop2006*100
+
+# possiamo creare una tabella = dataframe per fare gli istogrammi 
+# table con funzione data.frame(varie colonne con nomi delle colonne e valori)
+tabout <- data.frame(
+  class=c("Forest", "Human"),
+  perc1992=c(83, 17),
+  perc2006=c(45, 55)
+  )
+
+
+
