79bfe9e24904c541ada107a48d94f5a104d72dfb
Giulia Armillei <giuliaarm00@icloud.com>
2026-04-21 17:02:52 +0200

    Update 07_classification.R

--- Code/07_classification.R
+++ Code/07_classification.R
@@ -15,3 +15,23 @@ sunc <- im.classify(sun)
 sunc <- im.classify(sun, seed=3)
 sunc <- im.classify(sun, seed=42)
 
+# import Grand Canyon data
+can <- im.import("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+# classify Gran Canyon data
+cancc <- im.classify(can, num_cluster = 4, seed=42)
+
+# classifying data out of R
+setwd("C:/Users/Giulia/Downloads/drone")
+getwd()
+
+list.files()
+
+#import data
+sir <- rast("sirolo1.jpg")
+sir <- flip(sir)
+plot(sir)
+
+# classify data
+sirc <- im.classify(sir, num_clusters = 3)
+sirc <- im.classify(sir, num_clusters = 5)
