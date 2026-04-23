a38dbaf41b9862dc413a843560b88093bb72377e
Giulia Armillei <giuliaarm00@icloud.com>
2026-04-23 15:50:22 +0200

    Update Qrcode_generation.R

--- Code/Qrcode_generation.R
+++ Code/Qrcode_generation.R
@@ -1,4 +1,14 @@
 install.packages("qrcode")
 
 library(qrcode)
-url <- 
+
+url <- "https://github.com/giuliaarmillei"
+
+qr <- qr_code(url)
+
+setwd("C:/Users/Giulia/OneDrive - Alma Mater Studiorum Università di Bologna/Desktop")
+getwd()
+
+png("github_profile_qr.png", width = 1000, height = 1000)
+plot(qr)
+dev.off()
