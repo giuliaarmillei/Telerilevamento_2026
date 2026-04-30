6f490b2f65221a0d2b057d66ddf3f7b8179593e3
Giulia Armillei <giuliaarm00@icloud.com>
2026-04-30 14:44:47 +0200

    Update 08_functions.R

--- Code/08_functions.R
+++ Code/08_functions.R
@@ -10,3 +10,46 @@ differenza <- function(x,y){
   z=x-y
   return(z)
   }
+
+# par = creo la funzione par in modo da non dover scrivee tutto mfrow 
+mf <- function(nx,ny){
+  par(mfrow=c(nx,ny))
+  }
+
+# in questo caso creiamo la funzione par con già elementi di default di righe e colonne 
+mf2 <- function(nx=1, ny=2){
+  per(mfrow=c(nx,ny))
+  }
+
+# if else: se --> allora = è una funzione con messaggio che noi definiiamo. positivo(6) --> "Questo numero è positivo"
+
+positivo <- function(x){
+  if(x>0) {
+    "Questo numero è positivo"
+    }
+  else if(x<0) {
+    "Questo numero è negativo"
+    }
+  else {
+    "Zero non è negativo nè positivo"
+    }
+  }
+
+# ciclo for (utile se dobbiamo ripetere l'operazione più volte) = con questa funzione se poi scrivo loop() R mi restituisce tutti gli i cioè tutti i numeri da 1 a 50.
+
+loop <- function() {
+  for(i in 1:50) {
+    print(i)
+    }
+  }
+
+# ciclo for che mi restituisce l'operazione che scrivo
+loop2 <- function() {
+  for(i in 1:50) {
+  op <- i * 7
+  print(op)
+    }
+  }
+
+
+
