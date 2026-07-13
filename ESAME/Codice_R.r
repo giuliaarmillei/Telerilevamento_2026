05daac4fbd59048a1a0e02c0060469fa186a4885
Giulia Armillei <giuliaarm00@icloud.com>
2026-07-13 11:53:20 +0200

    Update Codice_R.r

--- ESAME/Codice_R.r
+++ ESAME/Codice_R.r
@@ -20,3 +20,30 @@ post <- rast("Postincendio_agosto2022.tif")
 plot(post)
 
 # Le immagini sono state esportate e salvate in formato .png
+
+# Composizione RGB a colori naturali
+im.multiframe(1,2) #suddivisione della finestra grafica in 1 riga e 2 colonne
+plotRGB(pre, r = 3, g = 2, b = 1, stretch = "lin", main = "Pre-incendio")
+plotRGB(post, r = 3, g = 2, b = 1, stretch = "lin", main = "Post-incendio")
+dev.off()
+
+# Visualizzazione delle singole bande del visibile e del vicino infrarosso pre e post incendio
+im.multiframe(2,4) #suddivisione della finestra grafica in 2 righe e 4 colonne
+# Pre-incendio (luglio2022)
+plot(pre[[1]], col = magma(100), main = "Pre-incendio, B2")
+plot(pre[[2]], col = magma(100), main = "Pre-incendio, B3")
+plot(pre[[3]], col = magma(100), main = "Pre-incendio, B4")
+plot(pre[[4]], col = magma(100), main = "Pre-incendio, B8")
+# Post-incendio (agosto2022)
+plot(post[[1]], col = magma(100), main = "Post-incendio, B2")
+plot(post[[2]], col = magma(100), main = "Post-incendio, B3")
+plot(post[[3]], col = magma(100), main = "Post-incendio, B4")
+plot(post[[4]], col = magma(100), main = "Post-incendio, B8")
+dev.off()
+
+
+
+
+
+
+
