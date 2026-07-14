f202ac5320417deaef036109b13d4c11bc9451ea
Giulia Armillei <giuliaarm00@icloud.com>
2026-07-14 12:58:06 +0200

    Update Codice_R.r

--- ESAME/Codice_R.r
+++ ESAME/Codice_R.r
@@ -27,14 +27,14 @@ plot(post2024)
 ## Le immagini sono state esportate e salvate in formato .png
 
 # Composizione RGB a colori naturali
-im.multiframe(1,3) #suddivisione della finestra grafica in 1 riga e 3 colonne
+im.multiframe(1, 3) #suddivisione della finestra grafica in 1 riga e 3 colonne
 plotRGB(pre, r = 3, g = 2, b = 1, stretch = "lin", main = "Pre-incendio")
 plotRGB(post, r = 3, g = 2, b = 1, stretch = "lin", main = "Post-incendio")
 plotRGB(post2024, r = 3, g = 2, b = 1, stretch = "lin", main = "Due anni dopo")
 dev.off()
 
 # Visualizzazione delle singole bande del visibile e del vicino infrarosso pre e post incendio
-im.multiframe(3,4) #suddivisione della finestra grafica in 3 righe e 4 colonne
+im.multiframe(3, 4) #suddivisione della finestra grafica in 3 righe e 4 colonne
 ## Pre-incendio (luglio2022)
 plot(pre[[1]], col = magma(100), main = "Pre-incendio, B2")
 plot(pre[[2]], col = magma(100), main = "Pre-incendio, B3")
@@ -56,28 +56,50 @@ dev.off()
 ## calcolo indici con funzione im.dvi del pacchetto imageRy
 dvi_pre <- im.dvi(pre, 4, 3)
 dvi_post <- im.dvi(post, 4, 3)
-## differenza DVI pre e post incendio
-dvi_diff <- dvi_post - dvi_pre
+dvi_post2024 <- im.dvi(post2024, 4, 3) 
 
-## visualizzazione indici per il confronto temporale
-im.multiframe(1,3) #suddivisione della finestra grafica in 1 riga e 3 colonne
+## visualizzazione indici 
+im.multiframe(1, 3) #suddivisione della finestra grafica in 1 riga e 3 colonne
 plot(dvi_pre, col = cividis(100), main = "DVI luglio 2022")
 plot(dvi_post, col = cividis(100), main = "DVI agosto 2022")
-plot(dvi_diff, col = cividis(100), main = "Differenza DVI")
+plot(dvi_post2024, col = cividis(100), main = "DVI agosto 2024")
+dev.off()
+
+## calcolo differenze indici dvi
+dvi_diff1 <- dvi_post - dvi_pre
+dvi_diff2 <- dvi_post2024 - dvi_post
+dvi_diff3 <- dvi_post2024 - dvi_pre
+
+## visualizzazione differenze indici per il confronto temporale
+im.multiframe(1, 3)
+plot(dvi_diff1, col = cividis(100), main = "luglio2022 - agosto2022")
+plot(dvi_diff2, col = cividis(100), main = "agosto2022 - agosto2024")
+plot(dvi_diff3, col = cividis(100), main = "luglio2022 - agosto2024")
 dev.off()
 
 # Analisi NDVI
 ## calcolo indici con la funzione im.ndvi del pacchetto imageRy
 ndvi_pre <- im.ndvi(pre, 4, 3)
 ndvi_post <- im.ndvi(post, 4, 3)
-## calcolo differenza indici ndvi
-ndvi_diff <- ndvi_post - ndvi_pre
+ndvi_post2024 <- im.ndvi(post2024, 4, 3)
 
-## visualizzazione indici e differenza indici per il confronto temporale
-im.multiframe(1,3) #suddivisione della finestra grafica in 1 riga e 3 colonne
+## visualizzazione indici 
+im.multiframe(1, 3) #suddivisione della finestra grafica in 1 riga e 3 colonne
 plot(ndvi_pre, col = inferno(100), main = "NDVI luglio 2022")
 plot(ndvi_post, col = inferno(100), main = "NDVI agosto 2022")
-plot(ndvi_diff, col = inferno(100), main = "Differenza NDVI")
+plot(ndvi_post2024, col = inferno(100), main = "NDVI agosto 2024")
+dev.off()
+
+## calcolo differenza inidici ndvi
+ndvi_diff1 <- ndvi_post - ndvi_pre
+ndvi_diff2 <- ndvi_post2024 - ndvi_post
+ndvi_diff3 <- ndvi_post2024 - ndvi_pre
+
+## visualizzazione differenze indici per il confronto temporale
+im.multiframe(1, 3) 
+plot(ndvi_diff1, col = inferno(100), main = "luglio2022 - agosto2022")
+plot(ndvi_diff2, col = inferno(100), main = "agosto2022 - agosto2024")
+plot(ndvi_diff3, col = inferno(100), main = "luglio2022 - agosto2024")
 dev.off()
 
 # Analisi multitemporale della distribuzione dell'NDVI mediante ridgeline plot
