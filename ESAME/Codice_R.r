83cee10f5fcceb42cb4d91244d62595cb9a53034
Giulia Armillei <giuliaarm00@icloud.com>
2026-07-17 12:01:16 +0200

    Update Codice_R.r

--- ESAME/Codice_R.r
+++ ESAME/Codice_R.r
@@ -42,21 +42,21 @@ plot(post2024[[1]], col = magma(100), main = "Due anni dopo, B2")
 plot(post2024[[2]], col = magma(100), main = "Due anni dopo, B3")
 plot(post2024[[3]], col = magma(100), main = "Due anni dopo, B4")
 plot(post2024[[4]], col = magma(100), main = "Due anni dopo, B8")
-dev.off()            #chiusura della finestra grafica
+
 
 # Composizione RGB a colori naturali
 im.multiframe(1, 3)   #suddivisione della finestra grafica in 1 riga e 3 colonne
 plotRGB(pre, r = 3, g = 2, b = 1, stretch = "lin", main = "Pre-incendio")
 plotRGB(post, r = 3, g = 2, b = 1, stretch = "lin", main = "Post-incendio")
 plotRGB(post2024, r = 3, g = 2, b = 1, stretch = "lin", main = "Due anni dopo")
-dev.off()             #chiusura della finestra grafica
+
 
 # Composizione RGB a falsi colori
 im.multiframe(1, 3)   #suddivisione della finestra grafica in 1 riga e 3 colonne
 plotRGB(pre, r = 4, g = 3, b = 2, stretch = "lin", main = "Pre-incendio")
 plotRGB(post, r = 4, g = 3, b = 2, stretch = "lin", main = "Post-incendio")
 plotRGB(post2024, r = 4, g = 3, b = 2, stretch = "lin", main = "Due anni dopo")
-dev.off() 
+
 
 # Analisi DVI
 ## calcolo indici con funzione im.dvi del pacchetto imageRy
@@ -69,7 +69,7 @@ im.multiframe(1, 3)   #suddivisione della finestra grafica in 1 riga e 3 colonne
 plot(dvi_pre, col = cividis(100), main = "DVI luglio 2022")
 plot(dvi_post, col = cividis(100), main = "DVI agosto 2022")
 plot(dvi_post2024, col = cividis(100), main = "DVI agosto 2024")
-dev.off()
+
 
 # Analisi NDVI
 ## calcolo indici con la funzione im.ndvi del pacchetto imageRy
@@ -82,7 +82,7 @@ im.multiframe(1, 3)   #suddivisione della finestra grafica in 1 riga e 3 colonne
 plot(ndvi_pre, col = inferno(100), main = "NDVI luglio 2022")
 plot(ndvi_post, col = inferno(100), main = "NDVI agosto 2022")
 plot(ndvi_post2024, col = inferno(100), main = "NDVI agosto 2024")
-dev.off()
+
 
 ## calcolo differenza inidici ndvi tra le diverse fasi
 ndvi_diff1 <- ndvi_post - ndvi_pre
@@ -94,7 +94,7 @@ im.multiframe(1, 3)   #suddivisione finestra grafica in 1 riga e 3 colonne
 plot(ndvi_diff1, col = inferno(100), main = "agosto2022 - luglio2022")
 plot(ndvi_diff2, col = inferno(100), main = "agosto2024 - agosto2022")
 plot(ndvi_diff3, col = inferno(100), main = "agosto2024 - luglio2022")
-dev.off()
+
 
 # Analisi multitemporale della distribuzione dell'NDVI mediante ridgeline plot
 stack_ndvi <- c(ndvi_pre, ndvi_post, ndvi_post2024)               #creazione dello stack dei tre indici NDVI 
@@ -131,7 +131,7 @@ legend("bottomleft",            #posizione della legenda in basso a sinistra
        title = "Classe"         #titolo delle legenda
        bg = "white",            #impostazione del colore dello sfondo
        xpd = TRUE)              #la legenda può essere disegnata fuori dall'area del grafico
-dev.off()
+
 
 #calcolo delle frequenze 
 f_pre <- freq(class_pre)
